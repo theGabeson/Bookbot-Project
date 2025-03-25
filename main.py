1ebe102032793097c2e3ac29508524b0c960cf88
archwell <gabethemighty@gmail.com>
2025-03-24 22:44:12 -0300

    Adding a new function: Counting characters in a text file.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import word_counter
+from stats import character_counter
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -6,8 +7,14 @@ def get_book_text(path_to_file):
     return file_contents
 
 def main():
+    #Getting the text from a file
     text = get_book_text("books/frankenstein.txt")
+    #Counting the number of words in the file
     word_count = word_counter(text)
     print(f"{word_count} words found in the document")
+    #counting the number of characters in each 
+    number_of_characters = character_counter(text)
+    print(number_of_characters)
+    
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,13 @@
 def word_counter(text):
     word_number = len(text.split())
     return word_number
+
+def character_counter(text):
+    lowercase_text = text.lower()
+    character_dict = {}
+    for c in lowercase_text:
+        if c in character_dict:
+            character_dict[c] += 1
+        else:
+            character_dict[c] = 1
+    return character_dict
