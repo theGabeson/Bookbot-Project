485ef742034e1b4137c550691f23e74777cee5b1
archwell <gabethemighty@gmail.com>
2025-03-24 22:07:53 -0300

    Fixing the import error.

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+from stats import word_counter
+
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
     return file_contents
 
 def main():
-    read_book = get_book_text("books/frankenstein.txt")
-    print(read_book)
-    
+    text = get_book_text("books/frankenstein.txt")
+    word_count = word_counter(text)
+    print(f"{word_count} words found in the document")
+
 main()
---
+++ stats.py
@@ -0,0 +1,3 @@
+def word_counter(text):
+    word_number = len(text.split())
+    return word_number
