79a0904147e7095a10b90a3abfbad7e52cef7758
archwell <gabethemighty@gmail.com>
2025-03-24 21:38:42 -0300

    Redoing everything!

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        file_contents = f.read()
+    return file_contents
+
 def main():
-	from stats import get_book_text
-	
-
-
+    read_book = get_book_text("books/frankenstein.txt")
+    print(read_book)
+    
 main()
