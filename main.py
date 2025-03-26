97daef841d16c1201337ce8e9d25af92f897aa12
archwell <gabethemighty@gmail.com>
2025-03-26 00:05:38 -0300

    Final Version!

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from stats import word_counter
 from stats import character_counter
-
+from stats import character_sorter
+import sys
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
@@ -8,13 +12,27 @@ def get_book_text(path_to_file):
 
 def main():
     #Getting the text from a file
-    text = get_book_text("books/frankenstein.txt")
+    path = sys.argv[1]
+    text = get_book_text(path)
+
     #Counting the number of words in the file
     word_count = word_counter(text)
-    print(f"{word_count} words found in the document")
-    #counting the number of characters in each 
-    number_of_characters = character_counter(text)
-    print(number_of_characters)
-    
 
+    #Counting the number of characters in each 
+    number_of_characters = character_counter(text)
+    sorted_characters = character_sorter(number_of_characters)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+
+    for char_dict in sorted_characters:
+        char = char_dict["char"]
+        count = char_dict["count"]
+        # Only print alphabetic characters
+        if char.isalpha():
+            print(f"{char}: {count}")
+    print("============= END ===============")
 main()
--- stats.py
+++ stats.py
@@ -11,3 +11,13 @@ def character_counter(text):
         else:
             character_dict[c] = 1
     return character_dict
+
+def sort_on(dict):
+    return dict["count"]
+
+def character_sorter(char_dict):
+    chars_list = []
+    for char, count in char_dict.items():
+        chars_list.append({"char": char, "count": count})
+    chars_list.sort(reverse=True, key=sort_on)
+    return chars_list
